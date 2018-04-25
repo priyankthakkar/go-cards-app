a30200e8e80a2c827b1ba4fab8f08c6c3c8b88ed
Priyank Thakkar <priyankthakkar@gmail.com>
2018-04-25 16:55:06 +1000

    Change Log: 1. Understaning the basic data structures like array and slice, difference between them, syntax and how to work with them. Changes By: Priyank Thakkar

--- main.go
+++ main.go
@@ -21,6 +21,28 @@ func main() {
 	// we are calling function newCard() here which returns string
 	card = newCard()
 	fmt.Println(card)
+
+	printState()
+
+	/**
+	* GO has two basic data structures, array and slice
+	* array and slice are strongly typed, so while defining them datatypes are must
+	* at a time, they can store only one type of items
+	* array is fixed in length, so while defining them size is must
+	* slice are similar to array but they are dynamic in size, they can shrink and grow
+	 */
+
+	// defining a slice of string with name cards
+	// we can add string directly or we can all a function which returns string
+	cards := []string{"Ace of Diamonds", newCard()}
+	fmt.Println(cards)
+
+	// to add new elements to slice we can use append function
+	// append doesn't modify the exisiting slice
+	// it takes an old slice as an input, takes the element to be added and
+	// returns a new slice
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
 }
 
 /**
